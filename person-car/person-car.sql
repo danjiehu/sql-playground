45ba9a26d0fda6ef29ccd2b4e1457527c33272a7
danjiehu <danjiehu@outlook.com>
2022-07-06 14:23:27 +0800

    created demo for using uuid, completed notes for psql extensions and looking up sequences

--- person-car/person-car.sql
+++ person-car/person-car.sql
@@ -1,6 +1,6 @@
 -- summary:
 -- 1. one-on-one relationship between tables
--- 2. assigning car to people
+-- 2. assigning car to people (creating a foreign key relationship)
 --  UPPDATE person SET car_id = 2 WHERE id = 1;
 -- 3. INNER JOIN: returns only the people who has cars
 --  SELECT * FROM person
@@ -18,7 +18,27 @@
 --  SELECT * FROM person 
 --  LEFT JOIN car ON car.id = person.car_id
 --  WHERE car.* IS NULL;
-
+-- 8. remove a car from a person: deleting foreign key
+-- ! before removing car: needs to make sure that no one is referencing the car id, either by updating car_id to other/null, or by deleting the person altogether; otherwise violates foreign key constraint.
+--  DELETE FROM person WHERE id = 9000;
+--  DELETE FROM car WHERE id = 13;
+-- 9. generate query results to CSV file
+--  \copy (SELECT * FROM person LEFT JOIN car ON car.id = person.car_id) TO '/Users/danjiehu/Downloads/results.csv' DELIMITER ',' CSV HEADER;
+-- ! need to be on the same line 
+-- 10. serial & sequences: view and reset manually 
+--  SELECT * FROM person_id_seq; -- check last value 
+--  SELECT nextval('person_id_seq'::regclass); -- invoke incrementing sequence manually 
+--  ALTER SEQUENCE person_id_seq RESTART WITH 10; -- set last sequence value manually
+-- 11. postgres extensions
+--  SELET * FROM pg_available _exntensions; -- view all available extensions for postgres
+-- a few you might need: 
+--     plv8 (writing javascript), 
+--     uuid-ossp (generate uuid), 
+--     hstore (data type for storing (key, value) pairs),
+--     sslinfo (view ssl certificate info),
+--     autoinc (function for autoincrementing fields) 
+-- etc.
+-- 12. using uuid - see person-car-uuid.sql
 
 -- creating car prior to creating person because person table will need to reference car table primary key
 create table car(
